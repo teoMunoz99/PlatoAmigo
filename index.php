2f6164a2beeb1ced9c3c3c741e8a95df2aee5e84
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-12-23 14:10:52 -0300

    modifique el aspecto del formulario de registro

--- index.php
+++ index.php
@@ -8,35 +8,40 @@ require_once 'php/encabezado.php';
 <body class='bg-color'>
     <main class="d-flex flex-column">
         <!--Titulo-->
-        <h1 class='fw-bold text-center bg-primary text-white p-3 rounded-5 rounded-top-0'><i class="bi bi-suit-heart-fill text-danger"></i> PlatoAmigo</h1>
-        <section class='flex-grow-1 d-flex flex-column justify-content-center'>
+        <h1 class='fw-bold text-center bg-primary text-white p-3 rounded-5 rounded-top-0'><i
+                class="bi bi-suit-heart-fill text-danger"></i> PlatoAmigo</h1>
+        <section class='flex-grow-1 d-flex flex-column justify-content-around'>
             <!--Banner bienvenida-->
             <article class="d-flex justify-content-center align-items-center">
                 <img class="img-fluid w-75" src="img/index/7853929.jpg" alt="ilustracion de un chef">
             </article>
             <!--texto-->
             <article class="text-center container">
-                <p class="fs-5">
+                <p class="fs-5 m-0">
                     Cada plato que compartes es una oportunidad para hacer una diferencia en la vida de alguien más.
                     Únete a nuestra comunidad y sé parte de un movimiento que va más allá de la comida:
                     una experiencia de dar que toca corazones y llena estómagos.
                 </p>
             </article>
             <!--Botones-->
-            <article class="d-flex flex-column justify-content-center align-items-center">
-                <a class="btn btn-primary rounded-pill boton py-3" href="./php/publicaciones.php">Ver platos<i class="bi bi-arrow-right-short"></i></a>
+            <article class="d-flex flex-column justify-content-center align-items-center my-2">
+                <a class="btn btn-primary rounded-pill boton py-3" href="./php/publicaciones.php">Ver platos<i
+                        class="bi bi-arrow-right-short"></i></a>
                 <a class="btn btn-primary rounded-pill boton py-3" href="php/registro.php">Registrarme</a>
             </article>
         </section>
     </main>
-</body>
-<footer>
-    <section class='container'>
-        <p class="text-center text-secondary">&copy; 2023 PlatoAmigo. Todos los derechos reservados. | <a href="politica-de-privacidad.html">Política de Privacidad</a> | <a href="terminos-y-condiciones.html">Términos y Condiciones</a></p>
-    </section>
-</footer>
+    <footer>
+        <section class='container'>
+            <p class="text-center text-secondary">&copy; 2023 PlatoAmigo. Todos los derechos reservados. | <a
+                    href="politica-de-privacidad.html">Política de Privacidad</a> | <a
+                    href="terminos-y-condiciones.html">Términos y Condiciones</a></p>
+        </section>
+    </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
--- php/registro.php
+++ php/registro.php
@@ -1,14 +1,17 @@
 <?php
 $ruta = '../';
-$color = '#f9e6df';
+$color = '#0d6efd';
 require_once 'encabezado.php';
 ?>
 
 <body class="bg-color">
-    <main class="d-flex justify-content-center align-items-end flex-column mb-3">
-        <section>
+    <main class="d-flex flex-column">
+        <!--Titulo-->
+        <h1 class='fw-bold text-center bg-primary text-white p-3 rounded-5 rounded-top-0'><i
+                class="bi bi-suit-heart-fill text-danger"></i> PlatoAmigo</h1>
+        <section class='flex-grow-1 d-flex flex-column justify-content-center'>
             <!--Banner bienvenida-->
-            <article class="d-flex justify-content-center">
+            <article class="d-flex justify-content-center align-items-center">
                 <img class="img-fluid w-75" src="../img/registro/femaleChef.png" alt="ilustracion de un chef">
             </article>
             <!--Texto bienvenida al registro-->
@@ -20,21 +23,21 @@ require_once 'encabezado.php';
                 </p>
             </article>
             <!--Formulario de registro-->
-            <article class="container">
-                <form class="bg-light border rounded-3 p-3" action="registroOk.php" method="post">
+            <article class="container mb-4">
+                <form class="bg-light border rounded-5 p-3" action="registroOk.php" method="post">
                     <fieldset>
-                        <legend class="text-center mb-4">Formulario de Registro</legend>
+                        <legend class="text-center mb-4 fw-bold">Formulario de Registro</legend>
                         <div class="mb-3">
                             <label for="name" class="form-label">Nombre del local*</label>
-                            <input type="text" class="form-control" name="nombre" id="name">
+                            <input type="text" class="form-control rounded-pill py-3" name="nombre" id="name">
                         </div>
                         <div class="mb-3">
                             <label for="direc" class="form-label">Dirección*</label>
-                            <input type="text" class="form-control" name="direccion" id="direc">
+                            <input type="text" class="form-control rounded-pill py-3" name="direccion" id="direc">
                         </div>
                         <div class="mb-3">
                             <label for="ubi">Localidad*</label>
-                            <select class="form-select" id="ubi" name="localidad" aria-label="Default select example">
+                            <select class="form-select rounded-pill py-3" id="ubi" name="localidad" aria-label="Default select example">
                                 <option selected>--Seleccionar--</option>
                                 <option value="1">San Miguel de Tucumán</option>
                                 <option value="2">Yerba Buena</option>
@@ -42,23 +45,81 @@ require_once 'encabezado.php';
                             </select>
                         </div>
                         <div class="mb-3">
-                            <label for="num" class="form-label">Tel/Cel-(opcional)</label>
-                            <input type="number" class="form-control" name="telefono" id="num">
+                            <label for="cel" class="form-label">Tel/Cel-(opcional)</label>
+                            <input type="number" class="form-control rounded-pill py-3" name="telefono" id="cel">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email*</label>
-                            <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="email" class="form-control rounded-pill py-3" name="email" id="exampleInputEmail1"
+                                aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
                             <label for="contra" class="form-label">Contraseña*</label>
-                            <input type="password" name="password" class="form-control" id="contra">
+                            <input type="password" name="password" class="form-control rounded-pill py-3" id="contra">
                         </div>
                     </fieldset>
-                    <button type="submit" class="btn btn-primary">Registrarme</button>
+                    <button type="submit" class="btn btn-primary rounded-pill py-3">Registrarme !</button>
                 </form>
             </article>
         </section>
     </main>
-<?php
-require_once 'pie.php';
-?>
+
+    <?php
+    require_once 'pie.php';
+    ?>
+
+    <?php /*<main class="d-flex justify-content-center align-items-end flex-column mb-3">
+   <section>
+       <!--Banner bienvenida-->
+       <article class="d-flex justify-content-center">
+           <img class="img-fluid w-75" src="../img/registro/femaleChef.png" alt="ilustracion de un chef">
+       </article>
+       <!--Texto bienvenida al registro-->
+       <article class="text-center container">
+           <h1 class="fw-bold">¡Bienvenido a PlatoAmigo!</h1>
+           <p class="fs-5">
+               ¿Sobró algo delicioso en tu restaurante o bar? Compártelo con quienes lo necesitan.
+               Regístrate y haz del compartir comida una experiencia que cambia vidas.
+           </p>
+       </article>
+       <!--Formulario de registro-->
+       <article class="container">
+           <form class="bg-light border rounded-3 p-3" action="registroOk.php" method="post">
+               <fieldset>
+                   <legend class="text-center mb-4">Formulario de Registro</legend>
+                   <div class="mb-3">
+                       <label for="name" class="form-label">Nombre del local*</label>
+                       <input type="text" class="form-control" name="nombre" id="name">
+                   </div>
+                   <div class="mb-3">
+                       <label for="direc" class="form-label">Dirección*</label>
+                       <input type="text" class="form-control" name="direccion" id="direc">
+                   </div>
+                   <div class="mb-3">
+                       <label for="ubi">Localidad*</label>
+                       <select class="form-select" id="ubi" name="localidad" aria-label="Default select example">
+                           <option selected>--Seleccionar--</option>
+                           <option value="1">San Miguel de Tucumán</option>
+                           <option value="2">Yerba Buena</option>
+                           <option value="3">Three</option>
+                       </select>
+                   </div>
+                   <div class="mb-3">
+                       <label for="num" class="form-label">Tel/Cel-(opcional)</label>
+                       <input type="number" class="form-control" name="telefono" id="num">
+                   </div>
+                   <div class="mb-3">
+                       <label for="exampleInputEmail1" class="form-label">Email*</label>
+                       <input type="email" class="form-control" name="email" id="exampleInputEmail1"
+                           aria-describedby="emailHelp">
+                   </div>
+                   <div class="mb-3">
+                       <label for="contra" class="form-label">Contraseña*</label>
+                       <input type="password" name="password" class="form-control" id="contra">
+                   </div>
+               </fieldset>
+               <button type="submit" class="btn btn-primary">Registrarme</button>
+           </form>
+       </article>
+   </section>
+</main> */?>
