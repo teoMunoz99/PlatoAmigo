69465f8dd1149a06e556e03d0ae1b6cf6003396f
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-12-24 17:10:57 -0300

    converti en boton el nav de la pag de registro

--- php/registro.php
+++ php/registro.php
@@ -7,8 +7,10 @@ require_once 'encabezado.php';
 <body class="bg-color">
     <main class="d-flex flex-column">
         <!--Titulo-->
-        <h1 class='fw-bold text-center bg-primary text-white p-3 rounded-5 rounded-top-0'><i
-                class="bi bi-suit-heart-fill text-danger"></i> PlatoAmigo</h1>
+        <a href="../index.php" class='text-decoration-none'>
+            <h1 class='fw-bold text-center bg-primary text-white p-3 rounded-5 rounded-top-0'><i
+                    class="bi bi-suit-heart-fill text-danger"></i> PlatoAmigo</h1>
+        </a>
         <section class='flex-grow-1 d-flex flex-column justify-content-center'>
             <!--Banner bienvenida-->
             <article class="d-flex justify-content-center align-items-center">
@@ -37,7 +39,8 @@ require_once 'encabezado.php';
                         </div>
                         <div class="mb-3">
                             <label for="ubi">Localidad*</label>
-                            <select class="form-select rounded-pill py-3" id="ubi" name="localidad" aria-label="Default select example">
+                            <select class="form-select rounded-pill py-3" id="ubi" name="localidad"
+                                aria-label="Default select example">
                                 <option value="1">San Miguel de Tucumán</option>
                                 <option value="2">Yerba Buena</option>
                                 <option value="3">Otro</option>
@@ -49,8 +52,8 @@ require_once 'encabezado.php';
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email*</label>
-                            <input type="email" class="form-control rounded-pill py-3" name="email" id="exampleInputEmail1"
-                                aria-describedby="emailHelp">
+                            <input type="email" class="form-control rounded-pill py-3" name="email"
+                                id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
                             <label for="contra" class="form-label">Contraseña*</label>
@@ -68,57 +71,57 @@ require_once 'encabezado.php';
     ?>
 
     <?php /*<main class="d-flex justify-content-center align-items-end flex-column mb-3">
-   <section>
-       <!--Banner bienvenida-->
-       <article class="d-flex justify-content-center">
-           <img class="img-fluid w-75" src="../img/registro/femaleChef.png" alt="ilustracion de un chef">
-       </article>
-       <!--Texto bienvenida al registro-->
-       <article class="text-center container">
-           <h1 class="fw-bold">¡Bienvenido a PlatoAmigo!</h1>
-           <p class="fs-5">
-               ¿Sobró algo delicioso en tu restaurante o bar? Compártelo con quienes lo necesitan.
-               Regístrate y haz del compartir comida una experiencia que cambia vidas.
-           </p>
-       </article>
-       <!--Formulario de registro-->
-       <article class="container">
-           <form class="bg-light border rounded-3 p-3" action="registroOk.php" method="post">
-               <fieldset>
-                   <legend class="text-center mb-4">Formulario de Registro</legend>
-                   <div class="mb-3">
-                       <label for="name" class="form-label">Nombre del local*</label>
-                       <input type="text" class="form-control" name="nombre" id="name">
-                   </div>
-                   <div class="mb-3">
-                       <label for="direc" class="form-label">Dirección*</label>
-                       <input type="text" class="form-control" name="direccion" id="direc">
-                   </div>
-                   <div class="mb-3">
-                       <label for="ubi">Localidad*</label>
-                       <select class="form-select" id="ubi" name="localidad" aria-label="Default select example">
-                           <option selected>--Seleccionar--</option>
-                           <option value="1">San Miguel de Tucumán</option>
-                           <option value="2">Yerba Buena</option>
-                           <option value="3">Three</option>
-                       </select>
-                   </div>
-                   <div class="mb-3">
-                       <label for="num" class="form-label">Tel/Cel-(opcional)</label>
-                       <input type="number" class="form-control" name="telefono" id="num">
-                   </div>
-                   <div class="mb-3">
-                       <label for="exampleInputEmail1" class="form-label">Email*</label>
-                       <input type="email" class="form-control" name="email" id="exampleInputEmail1"
-                           aria-describedby="emailHelp">
-                   </div>
-                   <div class="mb-3">
-                       <label for="contra" class="form-label">Contraseña*</label>
-                       <input type="password" name="password" class="form-control" id="contra">
-                   </div>
-               </fieldset>
-               <button type="submit" class="btn btn-primary">Registrarme</button>
-           </form>
-       </article>
-   </section>
+<section>
+  <!--Banner bienvenida-->
+  <article class="d-flex justify-content-center">
+      <img class="img-fluid w-75" src="../img/registro/femaleChef.png" alt="ilustracion de un chef">
+  </article>
+  <!--Texto bienvenida al registro-->
+  <article class="text-center container">
+      <h1 class="fw-bold">¡Bienvenido a PlatoAmigo!</h1>
+      <p class="fs-5">
+          ¿Sobró algo delicioso en tu restaurante o bar? Compártelo con quienes lo necesitan.
+          Regístrate y haz del compartir comida una experiencia que cambia vidas.
+      </p>
+  </article>
+  <!--Formulario de registro-->
+  <article class="container">
+      <form class="bg-light border rounded-3 p-3" action="registroOk.php" method="post">
+          <fieldset>
+              <legend class="text-center mb-4">Formulario de Registro</legend>
+              <div class="mb-3">
+                  <label for="name" class="form-label">Nombre del local*</label>
+                  <input type="text" class="form-control" name="nombre" id="name">
+              </div>
+              <div class="mb-3">
+                  <label for="direc" class="form-label">Dirección*</label>
+                  <input type="text" class="form-control" name="direccion" id="direc">
+              </div>
+              <div class="mb-3">
+                  <label for="ubi">Localidad*</label>
+                  <select class="form-select" id="ubi" name="localidad" aria-label="Default select example">
+                      <option selected>--Seleccionar--</option>
+                      <option value="1">San Miguel de Tucumán</option>
+                      <option value="2">Yerba Buena</option>
+                      <option value="3">Three</option>
+                  </select>
+              </div>
+              <div class="mb-3">
+                  <label for="num" class="form-label">Tel/Cel-(opcional)</label>
+                  <input type="number" class="form-control" name="telefono" id="num">
+              </div>
+              <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email*</label>
+                  <input type="email" class="form-control" name="email" id="exampleInputEmail1"
+                      aria-describedby="emailHelp">
+              </div>
+              <div class="mb-3">
+                  <label for="contra" class="form-label">Contraseña*</label>
+                  <input type="password" name="password" class="form-control" id="contra">
+              </div>
+          </fieldset>
+          <button type="submit" class="btn btn-primary">Registrarme</button>
+      </form>
+  </article>
+</section>
 </main> */?>
