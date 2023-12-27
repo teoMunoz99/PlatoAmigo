347a582a76c6d5b4ccc293dc7020a25e948cf452
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-12-27 19:58:08 -0300

    agregue a las ocpiones de localidad, todas las localidades de tucuman

--- php/registro.php
+++ php/registro.php
@@ -30,34 +30,50 @@ require_once 'encabezado.php';
                     <fieldset>
                         <legend class="text-center mb-4 fw-bold">Formulario de Registro</legend>
                         <div class="mb-3">
-                            <label for="name" class="form-label">Nombre del local*</label>
-                            <input type="text" class="form-control rounded-pill py-3" name="nombre" id="name">
+                            <label for="name" class="form-label">Nombre del local</label>
+                            <input type="text" class="form-control rounded-pill py-3" name="nombre" id="name" minlength="3" maxlength="15" required>
                         </div>
                         <div class="mb-3">
-                            <label for="direc" class="form-label">Dirección*</label>
-                            <input type="text" class="form-control rounded-pill py-3" name="direccion" id="direc">
+                            <label for="direc" class="form-label">Dirección</label>
+                            <input type="text" class="form-control rounded-pill py-3" minlength="5" maxlength="45" name="direccion" id="direc" required>
                         </div>
                         <div class="mb-3">
-                            <label for="ubi">Localidad*</label>
-                            <select class="form-select rounded-pill py-3" id="ubi" name="localidad"
+                            <label for="ubi">Localidad</label>
+                            <select class="form-select rounded-pill py-3" id="ubi" name="localidad"required
                                 aria-label="Default select example">
-                                <option value="1">San Miguel de Tucumán</option>
-                                <option value="2">Yerba Buena</option>
-                                <option value="3">Otro</option>
+                                <option value="San Miguel de Tucumán">San Miguel de Tucumán</option>
+                                <option value="Yerba Buena">Yerba Buena</option>
+                                <option value="Aguilares">Aguilares</option>
+                                <option value="Alderetes">Alderetes</option>
+                                <option value="Banda del Río Salí">Banda del Río Salí</option>
+                                <option value="Bella Vista">Bella Vista</option>
+                                <option value="Burruyacú">Burruyacú</option>
+                                <option value="Concepción">Concepción</option>
+                                <option value="Famaillá">Famaillá</option>
+                                <option value="Graneros">Graneros</option>
+                                <option value="Juan Bautista Alberdi">Juan Bautista Alberdi</option>
+                                <option value="La Cocha">La Cocha</option>
+                                <option value="Las Talitas">Las Talitas</option>
+                                <option value="Lules">Lules</option>
+                                <option value="Monteros">Monteros</option>
+                                <option value="Simoca">Simoca</option>
+                                <option value="Tafí del Valle">Tafí del Valle</option>
+                                <option value="Tafí Viejo">Tafí Viejo</option>
+                                <option value="Trancas">Trancas</option>
                             </select>
                         </div>
                         <div class="mb-3">
-                            <label for="cel" class="form-label">Tel/Cel-(opcional)</label>
-                            <input type="number" class="form-control rounded-pill py-3" name="telefono" id="cel">
+                            <label for="cel" class="form-label">Tel/Cel</label>
+                            <input type="number" class="form-control rounded-pill py-3" min="1000000" max="999999999999999" name="telefono" id="cel" required>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email*</label>
+                            <label for="exampleInputEmail1" class="form-label">Email</label>
                             <input type="email" class="form-control rounded-pill py-3" name="email"
-                                id="exampleInputEmail1" aria-describedby="emailHelp">
+                                id="exampleInputEmail1" aria-describedby="emailHelp" required>
                         </div>
                         <div class="mb-3">
-                            <label for="contra" class="form-label">Contraseña*</label>
-                            <input type="password" name="password" class="form-control rounded-pill py-3" id="contra">
+                            <label for="contra" class="form-label">Contraseña</label>
+                            <input type="password" minlength="8" maxlength="20" name="password" class="form-control rounded-pill py-3" id="contra" required>
                         </div>
                     </fieldset>
                     <button type="submit" class="btn btn-primary rounded-pill py-3">Registrarme !</button>
