9b2bd55575a21b1ccd2817dc058e51065b04c5bb
Tadeo Munoz <tadeomunoz09@hotmail.com>
2024-01-13 15:39:48 -0300

    cambie las paginas de exito y fallo para que puedan ser usadas en cualquier lado pasandoles un $msj como parametro

--- php/pages/fallo.php
+++ php/pages/fallo.php
@@ -3,11 +3,14 @@ $ruta = '../../';
 $color = '#f9e6df';
 require_once 'components/encabezado.php';
 ?>
+
 <body class="bg-color">
     <main class="d-flex justify-content-center align-items-center container">
         <section class="text-center">
             <i class="bi bi-x-circle-fill icono-check text-danger"></i>
-            <h1>Fallo al registrar/loguear, intente mas tarde</h1>
+            <h1>
+                <?php echo $msj; ?>
+            </h1>
             <p class="text-secondary">(Espere a ser redireccionado...)</p>
         </section>
     </main>
