ad6870b7c857169ee67b59f3e2a8c460cbe54738
Tadeo Munoz <tadeomunoz09@hotmail.com>
2024-01-14 15:08:11 -0300

    liste los locales disponibles en la pag de localesDisponibles, tambien ajuste las cards de locales para mostrar el contenido

--- php/pages/components/cardLocales.php
+++ php/pages/components/cardLocales.php
@@ -1,16 +1,20 @@
 <article class="card rounded-5 p-2 my-3">
     <div class="card-body d-flex">
         <div class='w-100 me-4'>
-            <h5 class="card-title"><?php echo $nombreL; ?></h5>
+            <h5 class="card-title text-center">
+                <?php echo $nombreL; ?>
+            </h5>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item"><span class='text-secondary'>Direccion:</span> <?php echo $direccionL.', '.$localidadL; ?></li>
-                <li class="list-group-item"><span class='text-secondary'>Tel/Cel:</span><?php echo $telL; ?></li>
+                <li class="list-group-item"><span class='text-secondary'>Direccion:</span>
+                    <?php echo $direccionL . ', ' . $localidadL; ?>
+                </li>
+                <li class="list-group-item"><span class='text-secondary'>Tel/Cel:</span>
+                    <?php echo $celL; ?>
+                </li>
             </ul>
+            <div class="d-flex justify-content-end">
+                <a href="#" class="btn btn-danger rounded-pill">Ver menú</a>
+            </div>
         </div>
-        <?php if(isset($_SESSION['tipo']) && $_SESSION['tipo'] == 'A'){ ?>
-        <div class='d-flex align-items-start'>
-            <a href="eliminarP.php?id=<?php echo $id_plato;?>" class=""><i class="bi bi-x-circle-fill fs-1 text-danger"></i></a>
-        </div>
-        <?php }?>
     </div>
 </article>
--- php/pages/localesDisponibles.php
+++ php/pages/localesDisponibles.php
@@ -20,64 +20,32 @@ require_once 'components/encabezado.php';
     <main class='container'>
         <!--Titulo-->
         <h2 class='text-center fs-1 fw-bold mt-4'>Locales disponibles</h2>
-        <!--Agregar platos-->
-        <?php
-        if (isset($_SESSION['user'])) {
-            ?>
-            <section class="d-flex justify-content-center mt-3">
-                <a href="agregarPlato.php" class='text-center text-decoration-none text-dark fw-bold'>
-                    <div class='btn btn-success m-1 btn-p rounded-4 d-flex justify-content-center align-items-center'>
-                        <i class="bi bi-plus icono-btn"></i>
-                    </div>
-                    Agregar
-                </a>
-            </section>
-        <?php } ?>
         <!--Publicaciones-->
-        <section class=''>
+        <section class='container'>
             <?php
             include '../functions/conexion.php';
             $conexion = conectar();
 
             if ($conexion) {
-                $consultaP = 'SELECT id_plato, nombre, descripcion, fecha, id_localP FROM platos';
-                $sentenciaP = mysqli_prepare($conexion, $consultaP);
+                $consultaL = 'SELECT id_local, nombre, direccion, localidad, cel, tipo FROM locales';
+                $sentenciaL = mysqli_prepare($conexion, $consultaL);
+                $q = mysqli_stmt_execute($sentenciaL);
 
-                $qP = mysqli_stmt_execute($sentenciaP);
-
-                if ($qP) {
-                    mysqli_stmt_bind_result($sentenciaP, $id_plato, $nombreP, $descripcionP, $fecha, $id_localP);
-                    mysqli_stmt_store_result($sentenciaP);
-
-                    $cantidadP = mysqli_stmt_num_rows($sentenciaP);
-
-                    if ($cantidadP > 0) {
-                        while (mysqli_stmt_fetch($sentenciaP)) {
-                            $consultaL = 'SELECT nombre, direccion, localidad, tipo FROM locales WHERE id_local = ?';
-                            $sentenciaL = mysqli_prepare($conexion, $consultaL);
-
-                            mysqli_stmt_bind_param($sentenciaL, 'i', $id_localP);
-                            $qL = mysqli_stmt_execute($sentenciaL);
-
-                            if ($qL) {
-                                mysqli_stmt_store_result($sentenciaL);
-
-                                $cantL = mysqli_stmt_num_rows($sentenciaL);
-
-                                if ($cantL > 0) {
-                                    mysqli_stmt_bind_result($sentenciaL, $nombreL, $direccionL, $localidadL, $tipo);
-                                    mysqli_stmt_fetch($sentenciaL);
-                                    include 'components/cardPlatos.php';
-                                }
-
-                                mysqli_stmt_free_result($sentenciaL);
+                if ($q) {
+                    mysqli_stmt_bind_result($sentenciaL, $id_local, $nombreL, $direccionL, $localidadL, $celL, $tipo);
+                    mysqli_stmt_store_result($sentenciaL);
+                    $cantL = mysqli_stmt_num_rows($sentenciaL);
+                    if ($cantL > 0) {
+                        while (mysqli_stmt_fetch($sentenciaL)) {
+                            if($tipo != 'A'){
+                            include 'components/cardLocales.php';
                             }
                         }
                     } else {
-                        echo '<p>No hay platos disponibles</p>';
+                        echo '<p>No hay locales disponibles</p>';
                     }
 
-                    mysqli_stmt_free_result($sentenciaP);
+                    mysqli_stmt_free_result($sentenciaL);
                 }
 
                 mysqli_close($conexion);
@@ -85,7 +53,6 @@ require_once 'components/encabezado.php';
                 echo 'Error en la conexión a la base de datos';
             }
             ?>
-
         </section>
     </main>
     <?php
